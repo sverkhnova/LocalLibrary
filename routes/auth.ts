f9e3448923bab67fb13d6c3e7beca93d7ff25ae5
sverkhnova <daniilnagasbekov@gmail.com>
2024-12-20 20:43:46 +0500

    try to solve problem

---
+++ routes/auth.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response } from "express";
+import { AppDataSource } from "../src/data-source";
+import { User } from "../src/entities/User";
+import bcrypt from "bcrypt";
+
+
+const router = express.Router();
+
+router.post("/register", async (req: Request, res: Response) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const userRepository = AppDataSource.getRepository(User);
+    const existingUser = await userRepository.findOne({ where: { email } });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email уже используется" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = userRepository.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await userRepository.save(newUser);
+
+    res.status(201).json({ message: "Пользователь успешно создан" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Ошибка на сервере" });
+  }
+});
+
+export default router;
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import "reflect-metadata";
 import { AppDataSource } from "./data-source";
+import express from "express";
+import authRoutes from "../routes/auth";
+
+const app = express();
 
 AppDataSource.initialize()
   .then(() => {
     console.log("📦 Подключено к базе данных PostgreSQL");
   })
   .catch((error) => console.error("❌ Ошибка подключения", error));
+
+  app.use(express.json());
+  app.use("/auth", authRoutes);
+
+  app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+  });
