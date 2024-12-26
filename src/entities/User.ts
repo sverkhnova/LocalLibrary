b919c736bd5463d08548ffe9695f50c2dbf9af81
sverkhnova <daniilnagasbekov@gmail.com>
2024-12-26 20:50:13 +0500

    nodemailer

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -14,6 +14,12 @@ export class User {
   @Column()
   password!: string; // Хешированный пароль
 
+  @Column({ nullable: true })
+  resetPasswordToken!: string | null;
+  
+  @Column({ type: "timestamp", nullable: true})
+  resetPasswordExpires!: Date | null;
+
   @Column({ default: 'user' })
   role!: string; // Роль пользователя (user или admin)
 }
